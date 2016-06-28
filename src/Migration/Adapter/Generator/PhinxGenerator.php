5d3564fd60c6f10641d728f4de95a17d3dd0d9ae
odan <odan@users.noreply.github.com>
2016-06-28 17:48:12 +0200

    Add table engine and comment support

--- src/Migration/Adapter/Generator/PhinxGenerator.php
+++ src/Migration/Adapter/Generator/PhinxGenerator.php
@@ -56,17 +56,72 @@ class PhinxGenerator implements GeneratorInterface
                     // create the table
                     $output[] = $this->getCreateTable($tableName);
                 }
+                if (isset($table['table']['engine'])) {
+                    $output[] = $this->getAlterTableEngine($tableName, $table['table']['engine']);
+                }
+                if (isset($table['table']['table_comment'])) {
+                    $output[] = $this->getAlterTableComment($tableName, $table['table']['table_comment']);
+                }
             }
         }
+        if (!empty($old['tables'])) {
+            foreach ($old['tables'] as $tableName => $table) {
+                if (!isset($new['tables'][$tableName])) {
+                    $output[] = $this->getDropTable($tableName);
+                }
+            }
+        }
+
         return $output;
     }
 
-    public function getCreateTable($table)
+    protected function getCreateTable($table)
     {
         return sprintf("%s\$this->table(\"%s\")->save();", $this->ind2, $table);
     }
 
-    public function getAddColumn($table, $column, $dataType)
+    protected function getDropTable($table)
+    {
+        return sprintf("%s\$this->dropTable(\"%s\");", $this->ind2, $table);
+    }
+
+    protected function getAlterTableEngine($table, $engine)
+    {
+        return sprintf("%s\$this->execute('ALTER TABLE `%s` ENGINE=%s;');", $this->ind2, $table, $engine);
+    }
+
+    protected function getAlterTableComment($table, $comment)
+    {
+        $commentSave = $this->slash($comment);
+        return sprintf("%s\$this->execute('ALTER TABLE `%s` COMMENT='%s';');", $this->ind2, $table, $commentSave);
+    }
+
+    /**
+     *
+     * @param type $string
+     * @return type
+     */
+    public function slash($string)
+    {
+        /**
+         * http://dev.mysql.com/doc/refman/5.7/en/string-literals.html
+         * \0	An ASCII NUL (X'00') character
+          \'	A single quote (“'”) character
+          \"	A double quote (“"”) character
+          \b	A backspace character
+          \n	A newline (linefeed) character
+          \r	A carriage return character
+          \t	A tab character
+          \Z	ASCII 26 (Control+Z); see note following the table
+          \\	A backslash (“\”) character
+          \%	A “%” character; see note following the table
+          \_
+         */
+        //return addcslashes($string, '\0\'"\b\n\r\t\Z\\%_');
+        return addslashes($string);
+    }
+
+    protected function getAddColumn($table, $column, $dataType)
     {
         return sprintf("%s\$this->table(\"%s\")->addColumn('%s', '%s', '%s')->save();", $this->ind2, $table, $column, $dataType);
     }
