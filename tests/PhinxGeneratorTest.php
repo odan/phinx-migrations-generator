22c673b351c11dca092811dcedc1d6633f285e83
odan <odan@users.noreply.github.com>
2016-09-15 20:22:06 +0200

    Update test

--- tests/PhinxGeneratorTest.php
+++ tests/PhinxGeneratorTest.php
@@ -1,9 +1,12 @@
 <?php
 
-use Odan\Migration\Adapter\Generator\PhinxGenerator;
+use PDO;
+use Odan\Migration\Adapter\Database\MySqlAdapter;
+use Odan\Migration\Adapter\Generator\PhinxMySqlGenerator;
+use Symfony\Component\Console\Output\NullOutput;
 
 /**
- * @coversDefaultClass \Odan\Migration\Adapter\Generator\PhinxGenerator
+ * @coversDefaultClass \Odan\Migration\Adapter\Generator\PhinxMySqlGenerator
  */
 class GenerateMigrationTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,7 +18,10 @@ class GenerateMigrationTest extends \PHPUnit_Framework_TestCase
      */
     public function testGenerate()
     {
-        $gen = new PhinxGenerator();
+        $output = new NullOutput();
+        $pdo = $this->getPdo($this->getSettings());
+        $dba = new MySqlAdapter($pdo, $output);
+        $gen = new PhinxMySqlGenerator($dba, $output);
         $diff = $this->read(__DIR__ . '/diffs/newtable.php');
         $actual = $gen->createMigration('MyNewMigration', $diff);
         $expected = file_get_contents(__DIR__ . '/diffs/newtable_expected.php');
@@ -32,4 +38,44 @@ class GenerateMigrationTest extends \PHPUnit_Framework_TestCase
     {
         return require $filename;
     }
+
+    /**
+     * Get Db
+     *
+     * @param array $settings
+     * @return PDO
+     */
+    public function getPdo($settings)
+    {
+        $options = array_replace_recursive($settings['options'], [
+            // Enable exceptions
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            // Convert column names to lower case.
+            PDO::ATTR_CASE => PDO::CASE_LOWER,
+            // Set default fetch mode
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+        $pdo = new PDO($settings['dsn'], $settings['username'], $settings['password'], $options);
+        return $pdo;
+    }
+
+    /**
+     * Get settings for test database.
+     *
+     * @return array
+     */
+    public function getSettings()
+    {
+        return array(
+            'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+            'username' => 'root',
+            'password' => '',
+            'options' => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            ),
+            'schema_file' => __DIR__ . '/schema.php',
+            'migration_path' => __DIR__
+        );
+    }
+
 }
--- tests/diffs/newtable_expected.php
+++ tests/diffs/newtable_expected.php
@@ -1,11 +1,20 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class MyNewMigration extends AbstractMigration
 {
     public function change()
     {
         $this->table("newtable")->save();
+        $this->execute("ALTER TABLE `newtable` ENGINE='InnoDB';");
+        $this->execute("ALTER TABLE `newtable` COMMENT='';");
+        if($this->table('newtable')->hasColumn('id')) {
+            $this->table("newtable")->changeColumn('id', 'integer', array('null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'comment' => "sdfsdf"))->update();
+        } else {
+            $this->table("newtable")->addColumn('id', 'integer', array('null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'comment' => "sdfsdf"))->update();
+        }
+
     }
 }
