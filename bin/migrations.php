b66795180fa97de23c5db9326176452a301b7424
odan <odan@users.noreply.github.com>
2016-09-16 13:36:07 +0200

    Update autoloader

--- bin/migrations.php
+++ bin/migrations.php
@@ -2,7 +2,7 @@
 <?php
 // Debugging: SET XDEBUG_CONFIG=idekey=netbeans-xdebug
 if (!(class_exists('Symfony\\Component\\Console\\Application') && class_exists('Odan\\Migration\\Command\\GenerateCommand'))) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 }
 
 use Odan\Migration\Command\GenerateCommand;
