75725491c11eef1cb5f5a08907ccbd7415107303
odan <odan@users.noreply.github.com>
2017-03-07 19:46:08 +0100

    Updated example config file

--- bin/phinx-migrations-config.php
+++ bin/phinx-migrations-config.php
@@ -1,11 +1,13 @@
 <?php
 
 return array(
-    'dsn' => 'mysql:host=127.0.0.1;dbname=test',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4',
     'username' => 'root',
     'password' => '',
     'options' => array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => false,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
     ),
     'schema_file' => __DIR__ . '/schema.php',
     'migration_path' => __DIR__
