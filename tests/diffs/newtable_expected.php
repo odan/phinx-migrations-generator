d16aea4d4eb50d926c6a23b6412b057f2f32c594
odan <odan@users.noreply.github.com>
2018-07-09 18:12:44 +0200

    Fixed test #37

--- tests/diffs/newtable_expected.php
+++ tests/diffs/newtable_expected.php
@@ -8,7 +8,7 @@ class MyNewMigration extends AbstractMigration
     public function change()
     {
         $table = $this->table("newtable", ['id' => false, 'primary_key' => ["id"], 'engine' => "InnoDB", 'encoding' => "utf8", 'collation' => "utf8_general_ci", 'comment' => ""]);
-        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'identity' => 'enable']);
+        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'identity' => 'enable'])->save();
         $table->save();
     }
 }
