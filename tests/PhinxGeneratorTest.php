3d8c939e845c7165042fcc4dae40562c068b1c24
odan <odan@users.noreply.github.com>
2018-10-08 22:18:38 +0200

    Updated test

--- tests/PhinxGeneratorTest.php
+++ tests/PhinxGeneratorTest.php
@@ -60,17 +60,8 @@ class PhinxGeneratorTest extends TestCase
     {
         $this->execSql("CREATE TABLE `accounts` (
           `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-          `type` int(11) unsigned NOT NULL,
-          `status` int(11) unsigned NOT NULL DEFAULT '1',
-          `username` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-          `pwhash` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-          `staff_level` int(11) unsigned NOT NULL DEFAULT '1',
-          `display` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-          `logo` tinyint(4) DEFAULT '0',
-          PRIMARY KEY (`id`),
-          UNIQUE KEY `username_ui` (`username`),
-          KEY `type_i` (`type`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=DYNAMIC;");
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;");
         $oldSchema = $this->getTableSchema('accounts');
         $this->migrate();
 
