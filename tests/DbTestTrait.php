b693511ca0c8449cec082dd36680577b534387cb
odan <odan@users.noreply.github.com>
2018-11-24 11:10:36 +0100

    Fixed tests

--- tests/DbTestTrait.php
+++ tests/DbTestTrait.php
@@ -17,6 +17,8 @@ trait DbTestTrait
 {
     protected $pdo;
 
+    static $counter;
+
     /**
      * Call this template method before each test method is run.
      *
@@ -170,7 +172,8 @@ trait DbTestTrait
             unlink($file);
         }
 
-        $number = date('YmdHisu');
+        // must be really unique
+        $number = date('YmdHisu') . '_' . ++static::$counter . '_' . $this->uuid();
 
         // generate
         $application = new Application();
@@ -220,5 +223,36 @@ trait DbTestTrait
         }
 
         return true;
+    }
+
+    /**
+     * Returns a `UUID` v4 created from a cryptographically secure random value.
+     *
+     * @see https://www.ietf.org/rfc/rfc4122.txt
+     *
+     * @throws Exception
+     *
+     * @return string RFC 4122 UUID
+     */
+    protected function uuid(): string
+    {
+        return sprintf(
+            '%04x%04x%04x%04x%04x%04x%04x%04x',
+            // 32 bits for "time_low"
+            random_int(0, 65535),
+            random_int(0, 65535),
+            // 16 bits for "time_mid"
+            random_int(0, 65535),
+            // 12 bits before the 0100 of (version) 4 for "time_hi_and_version"
+            random_int(0, 4095) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            random_int(0, 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            random_int(0, 65535),
+            random_int(0, 65535),
+            random_int(0, 65535)
+        );
     }
 }
